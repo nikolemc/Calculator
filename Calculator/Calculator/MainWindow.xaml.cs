dc023068630374fcb4532e503021974c0f76114c
nikolemc <nikolemcstanley@gmail.com>
2017-03-30 14:27:49 -0400

    Basic calculator with 2 values text boexes where you can add, subtract, divide, multiply and mod

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)  //CLick on the Add Button 
+        {
+            int output = Int32.Parse(Value1.Text) + Int32.Parse(Value2.Text);  //Add the value from Text box 1 and add with Text Box 2 
+
+            Result.Text = output.ToString();  //Take the "output" value from line above and push that value into my Result text box
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            int output = Int32.Parse(Value1.Text) - Int32.Parse(Value2.Text);  //Subtract the value from Text box 1 and add with Text Box 2 
+
+            Result.Text = output.ToString();  //Take the "output" value from line above and push that value into my Result text box
+
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            int output = Int32.Parse(Value1.Text) * Int32.Parse(Value2.Text);  //Multiply the value from Text box 1 and add with Text Box 2 
+
+            Result.Text = output.ToString();  //Take the "output" value from line above and push that value into my Result text box
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            int output = Int32.Parse(Value1.Text) / Int32.Parse(Value2.Text);  //Divide the value from Text box 1 and add with Text Box 2 
+
+            Result.Text = output.ToString();  //Take the "output" value from line above and push that value into my Result text box
+        }
+
+        private void Mod_Click(object sender, RoutedEventArgs e)
+        {
+            int output = Int32.Parse(Value1.Text) % Int32.Parse(Value2.Text);  //Mod the value from Text box 1 and add with Text Box 2 
+
+            Result.Text = output.ToString();  //Take the "output" value from line above and push that value into my Result text box
+
+        }
     }
 }
+ 
